ca6cb1843297a4176d29f0fbe8672700eb2c3aa8
Max Zamkovoy <max.zamkovoy@gmail.com>
2019-07-23 19:45:18 +0300

    Initial solution for task 0313

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0313/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task03.task0313;
+
+/* 
+Мама мыла раму
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String a = "Мама";
+        String b = "Мыла";
+        String e = "Раму";
+        System.out.println(a+b+e);
+        System.out.println(b+e+a);
+        System.out.println(e+a+b);
+        System.out.println(e+b+a);
+        System.out.println(a+e+b);
+        System.out.println(b+a+e);
+    }
+}
