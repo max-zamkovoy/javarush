0ed1eada292344370a1ce0a5da577662b2d2eaa0
Max Zamkovoy <max.zamkovoy@gmail.com>
2019-07-24 09:25:46 +0300

    Final solution for task 0313
    Resolved using recursion. Thanks to Alex Fedorov!

--- 1.JavaSyntax/src/com/javarush/task/task03/task0313/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0313/Solution.java
@@ -2,18 +2,40 @@ package com.javarush.task.task03.task0313;
 
 /* 
 Мама мыла раму
+
+Уж не знаем, какой садист из авторов учебников для младших классов впервые заставил маму мыть злополучную раму, но фраза эта популярна не меньше, чем «Буря мглою небо кроет». Мы в секретном центре JavaRush классику уважаем. И комбинаторику тоже уважаем. Отсюда и задание: выведите все возможные комбинации слов «Мама», «Мыла» и «Раму».
 */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
+
     public static void main(String[] args) {
-        String a = "Мама";
-        String b = "Мыла";
-        String e = "Раму";
-        System.out.println(a+b+e);
-        System.out.println(b+e+a);
-        System.out.println(e+a+b);
-        System.out.println(e+b+a);
-        System.out.println(a+e+b);
-        System.out.println(b+a+e);
+        String[] patter = {"Мама", "Мыла", "Раму"};
+        for (String line : compileLines(new ArrayList<>(Arrays.asList(patter)))) {
+            System.out.println(line);
+        }
+    }
+
+    public static List<String> compileLines(List<String> words) {
+        List<String> result = new ArrayList<>();
+        if (words.size() == 1) {
+            result.add(words.get(0));
+            return result;
+        }
+        for (String word : words) {
+            for (String line : compileLines(getSubList(words, word))) {
+                result.add(word + line);
+            }
+        }
+        return result;
+    }
+
+    public static List<String> getSubList(List<String> words, String leftWord) {
+        List<String> subList = new ArrayList<>(words);
+        subList.remove(leftWord);
+        return subList;
     }
 }
